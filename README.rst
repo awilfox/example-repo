304c4e0912798d109e61574386a673d64e31cca2
A. Wilcox <AWilcox@Wilcox-Tech.com>
2022-02-17 22:10:30 -0600

    Initial commit

---
+++ README.rst
@@ -0,0 +1,14 @@
+=========================
+ README for Example Repo
+=========================
+
+:Author:
+  A Wilcox
+:Status:
+  Development
+
+
+Introduction
+============
+
+This is an example repository for helping people learn Git concepts.
---
+++ example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Example Python module.
+
+This module simply prints a nice message when executed."""
+
+
+def say_hi():
+    """Say hello to the user."""
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    say_hi()
